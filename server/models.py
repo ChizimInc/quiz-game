fd4a8032485812be4ed7ea1551778beeb1b05063
ChizimInc <chizimcezar@usa.com>
2021-03-27 19:09:03 +0200

    Add to server user_create, get_all_users, get_all_games

--- server/models.py
+++ server/models.py
@@ -1,7 +1,50 @@
-from pydantic import BaseModel
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
-class game(BaseModel):
-    title: str
-    owner: str
-    date: str
-    questions_count: int
+from db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
+
+    items = relationship("Item", back_populates="owner")
+
+
+class Item(Base):
+    __tablename__ = "games-items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    question = Column(String, index=True)
+
+    questions = relationship('Question', back_populates="questionOwner")
+
+    owner = relationship("User", back_populates="items")
+
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    questionOwner_id = Column(Integer, ForeignKey("games-items.id"))
+    answers = relationship("Answer", back_populates="answerOwner")
+
+    questionOwner = relationship("Item", back_populates="questions")
+
+class Answer(Base):
+    __tablename__ = "answers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    answerOwner_id = Column(Integer, ForeignKey("questions.id"))
+
+    answerOwner = relationship("Question", back_populates="answers")
